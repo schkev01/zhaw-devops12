dd318b8ebdb8b7271affb90082287506c5512842
schkev01 <84243281+schkev01@users.noreply.github.com>
2022-05-30 17:29:47 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 'use strict';
+
 const express = require('express');
+
 // Constants
 const PORT = process.env.PORT || 8080;
 const HOST = '0.0.0.0';
+
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello DevOps Course :-)))');
+  res.send('DevOpsHerokuDocker - Hello DevOps Course :-)))');
 });
+
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
